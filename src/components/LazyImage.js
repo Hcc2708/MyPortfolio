829239377fd5abb5fa7cce627dac3b9b4ea60fee
hemchand <hemuchandravanshi03@gmail.com>
2023-08-12 23:16:49 +0530

    ..

--- src/components/LazyImage.js
+++ src/components/LazyImage.js
@@ -5,7 +5,7 @@ export default function LazyImage({ src, alt, dataSrc }) {
     const options = {
       root: null,
       rootMargin: "0px",
-      threshold: 0.1,
+      threshold: 0.05,
     };
 
     const handleIntersection = (entries, observer) => {
